bdce971abde510ccebe2e2b9a4d1908809dcc423
drewminns <drew.minns@me.com>
2015-08-18 11:39:55 -0400

    updated xray build

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
-var express = require('express');
-var app = express();
+var express = require('express'),
+		app = express(),
+		morgan = require('morgan'),
+		routes = require('./path.js');
+
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+    res.header('Access-Control-Allow-Credentials', true);
+  	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, token");
+  	res.header('Access-Control-Allow-Methods', 'GET');
+    next();
+});
+
 // views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
+// app.get('/', function(req, res) {
+//   res.render('pages/index');
+// });
+
+// app.get('/api', function(req, res) {
+// 	res.jsonp({success: "hello"});
+// });
+
+app.get('/', routes.home);
+app.get('/api', routes.getListings);
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
---
+++ path.js
@@ -0,0 +1,74 @@
+var Xray = require('x-ray'),
+		x = Xray(),
+		api = {};
+
+api.data = [];
+
+api.home = function(req, res) {
+	res.render('pages/index');
+}
+
+api.getListings = function (req, res) {
+	res.contentType('application/json');
+	if (api.data.length > 2) {
+		api.data.length = [];
+	}
+
+	var location = req.query.location,
+			price = req.query.price,
+			rooms = req.query.rooms;
+	if (location === undefined || price === undefined || rooms === undefined) {
+		res.json({ error: 'Parameters not included'});
+	} else {
+
+		var kjURL, clURL;
+		if (location.indexOf(' ') >= 0) {
+			kjURL = 'http://www.kijiji.ca/b-' + rooms + '-bedroom-apartments-condos/city-of-toronto/' + location.replace(/\s/g, '-') + '/k0c212l1700273?price=__' + price;
+			clURL = 'http://toronto.craigslist.ca/search/apa?hasPic=1&maxAsk=' + price + '&bedrooms=' + rooms + '&query=' + location.replace(/\s/g, '+') + '#pic';
+		} else {
+			kjURL = 'http://www.kijiji.ca/b-' + rooms + '-bedroom-apartments-condos/city-of-toronto/' + location.replace('+', '-') + '/k0c212l1700273?price=__' + price;
+			clURL = 'http://toronto.craigslist.ca/search/apa?maxAsk=' + price + '&bedrooms=' + rooms + '&query=' + location + '#pic';
+		}
+
+		function CLbuild(url) {
+			x(url, 'p.row', [{
+			  title: '.txt .hdrlnk',
+			  link: '.txt a.hdrlnk@href',
+			  posted: '.pl time@title',
+			  price: '.txt .price',
+			  pic: x('.txt a.hdrlnk@href', '.body img@src')
+			}])
+			(function(err, obj) {
+				if (err)
+					res.send(err);
+					console.log(obj);
+				 api.data.push(obj);
+				 KJbuild(kjURL);
+			});
+		}
+
+		function KJbuild(url) {
+			x(url, '.regular-ad', [{
+				title: '.description a.title',
+				link: '.description a@href',
+				posted: '.posted',
+				price: '.price',
+				pic: x('.description a@href', 'li.showing img@src')
+			}])
+			(function(err, obj) {
+				if (err)
+					res.send(err);
+				console.log(obj)
+				 api.data.push(obj)
+				 var a = api.data[0];
+				 var b = api.data[1];
+				 var c = a.concat(b);
+				 res.jsonp(c);
+			});
+		}
+
+		CLbuild(clURL);
+	}
+}
+
+module.exports = api;
